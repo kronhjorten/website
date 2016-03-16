c76f2d0cfa21cd812d6f16d4de4d95402d041663
Glen Jensen <dooffie@gmail.com>
2016-03-16 13:13:20 +0100

    added additional files.

---
+++ kronhjorten/kronhjorten/js/Slide.js
@@ -0,0 +1,8 @@
+﻿$(function () {
+    $('a').click(function () {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 1000);
+        return false;
+    });
+});
